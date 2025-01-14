8cd19ebc16abbae16f78b0abb277210533e4de7f
Alexander Baker <alex.jbaker20@googlemail.com>
2025-01-14 18:33:36 +0000

    maint - secret key config

--- build_mt/settings.py
+++ build_mt/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ls0-qhh6uung!^(kzsq-+ghn$4!m1f4%19s%wm9l8*&njst4k)'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
