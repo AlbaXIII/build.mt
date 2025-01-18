a7e9abae56b734225ccbc3375c4bd50a95d00c90
Alexander Baker <alex.jbaker20@googlemail.com>
2025-01-18 16:50:24 +0000

    feat - add search functionality to build list & redirect to results

--- bgbuild/urls.py
+++ bgbuild/urls.py
@@ -1,7 +1,8 @@
 from . import views
 from django.urls import path
-from .views import BgbuildList
+from .views import BgbuildList, searchBuild
 
 urlpatterns = [
     path('', BgbuildList.as_view(), name="bgbuilds"),
-]
+    path('q/', searchBuild, name='searchbuild'),
+] 
--- bgbuild/views.py
+++ bgbuild/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
 from django.views.generic import CreateView, ListView, DeleteView, UpdateView
+from django.db.models import Q
 from .models import Bgbuild
 
 # Create your views here.
 class BgbuildList(generic.ListView):
     queryset = Bgbuild.objects.all().order_by("-created_on")
     template_name = "bgbuild/bgbuild_list.html"
+
+def searchBuild(request):
+    if request.method == "POST":
+        build_search = request.POST.get('searchbuilds')
+        builds = Bgbuild.objects.filter(bgbase_class__contains=build_search) | Bgbuild.objects.filter(bgbuild_role__contains=build_search)
+        total = builds.count()
+        return render(request, 'bgbuild/bgbuild_search.html', {'build_search': build_search, 'builds':builds, 'total': total,})
+    else:
+        return render(request, 'bgbuild/bgbuild_search.html', {})
+
