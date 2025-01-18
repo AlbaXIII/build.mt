a0bd4d55042be4a84779b6026f29eea58241f7d3
Alexander Baker <alex.jbaker20@googlemail.com>
2025-01-18 12:24:33 +0000

    feat - add bgbuildlist page, css styles and favicon

--- bgbuild/urls.py
+++ bgbuild/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.urls import path
+from .views import BgbuildList
 
 urlpatterns = [
-    path('', views.BgbuildList.as_view(), name='bgbuild'),
+    path('', BgbuildList.as_view(), name="bgbuilds"),
 ]
--- bgbuild/views.py
+++ bgbuild/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic import CreateView, ListView, DeleteView, UpdateView
 from .models import Bgbuild
 
 # Create your views here.
 class BgbuildList(generic.ListView):
-    model = Bgbuild
+    queryset = Bgbuild.objects.all().order_by("-created_on")
+    template_name = "bgbuild/bgbuild_list.html"
