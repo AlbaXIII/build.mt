97d6d36287038339173f5dad5238bb8e5aac5994
Alexander Baker <alex.jbaker20@googlemail.com>
2025-01-19 15:48:20 +0000

    feat - added allauth authentication & custom login/out pages

--- bgbuild/admin.py
+++ bgbuild/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Bgbuild
 
+@admin.register(Bgbuild)
+
+class BgbuildAdmin(SummernoteModelAdmin):
+    list_display = ('bgbuild_title', 'slug', 'status', 'created_on')
+    search_fields = ['bgbuild_title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('bgbuild_title',)}
+    summernote_fields = ('content',)
+
 # Register your models here.
-admin.site.register(Bgbuild)
--- build_mt/urls.py
+++ build_mt/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path("bgbuild/", include("bgbuild.urls"), name="bgbuild-urls"),
+    path('summernote/', include('django_summernote.urls')),
 ]
