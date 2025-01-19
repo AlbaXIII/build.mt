7e59205acb86013c311c7f46a291e3a6a0748761
Alexander Baker <alex.jbaker20@googlemail.com>
2025-01-19 13:56:45 +0000

    feat - add if tags to display relevant subclasses

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+const multiclassActive = document.getElementById("multiclass-active");
+const multiclassOne = document.getElementById("multiclass-one");
+const multiclassLevel = document.getElementById("multiclass-level");
+
+if (multiclassActive.innertext = ("No")){
+    multiclassOne.style.visibility = 'hidden';
+    multiclassLevel.style.visibility = 'hidden';
+}
+
+
