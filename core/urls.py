457a5b6ee9593eea1bd5c180fbf89c159ef0e0c7
Alexander Baker <alex.jbaker20@googlemail.com>
2025-01-27 15:19:49 +0000

    feat - add account page to core, show builds submit by auth user

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home, UserBuilds
 
 urlpatterns = [
-    path('', Home.as_view(), name='home')
+    path('', Home.as_view(), name='home'),
+    path('account/', UserBuilds.as_view(), name='account')
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from bgbuild.models import Bgbuild
 
 # Create your views here.
 
@@ -9,5 +10,19 @@ class Home(TemplateView):
     Class based view to render home page.
     """
     template_name = 'core/index.html'
+
+class UserBuilds(ListView):
+    
+    model = Bgbuild
+    template_name = 'core/account.html'
+    context_object_name = 'mybuilds'
+
+    def get_queryset(self):
+        """
+        Return a queryset of builds created by the logged-in user,
+        ordered by creation date in descending order.
+        """
+        return Bgbuild.objects.filter(
+            user=self.request.user).order_by('-created_on')
 
 
