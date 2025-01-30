700d1e3352aaa0d7ee52e993987fae3d83025a0b
AlbaXIII <alex.jbaker20@googlemail.com>
2025-01-30 11:57:24 +0000

    maint - complete migration to vscode

--- build_mt/settings.py
+++ build_mt/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-albaxiii-buildmt-4r2au04hxk3.ws.codeinstitute-ide.net', '.herokuapp.com',]
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com',]
 
 
 # Application definition
@@ -108,7 +108,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.codeinstitute-ide.net/",
+    "https://localhost",
     "https://*.herokuapp.com"
 ]
 
